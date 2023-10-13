1e9120a303e56174c528995a895df9534d7b55e5
marela <ma52368@fer.hr>
2023-10-13 16:09:26 +0200

    proper init

---
+++ BACK/web2_lab1/src/main/java/com/example/web2_lab1/Web2Lab1Application.java
@@ -0,0 +1,13 @@
+package com.example.web2_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2Lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2Lab1Application.class, args);
+	}
+
+}
---
+++ BACK/web2_lab1/src/test/java/com/example/web2_lab1/Web2Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2Lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
