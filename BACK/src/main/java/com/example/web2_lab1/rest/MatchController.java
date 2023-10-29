9c9547ef855e263e1dd183f12ce2f28f2810d790
marela <ma52368@fer.hr>
2023-10-29 20:52:06 +0100

    aaaa

--- BACK/src/main/java/com/example/web2_lab1/rest/MatchController.java
+++ BACK/src/main/java/com/example/web2_lab1/rest/MatchController.java
@@ -1,12 +1,12 @@
 package com.example.web2_lab1.rest;
 
-import com.example.web2_lab1.domain.request.NewCompetitionRequest;
 import com.example.web2_lab1.domain.request.SaveMatchRequest;
 import com.example.web2_lab1.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @CrossOrigin(origins = "*")
